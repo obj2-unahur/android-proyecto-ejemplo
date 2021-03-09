32838d725348229c67142217b3afbeef39c9e717
Rodrigo Alfonso <rodri042@gmail.com>
2021-03-08 23:42:07 -0300

    Usamos DescribeSpec en vez de StringSpec

--- app/src/test/java/com/example/unahur/ContadorTest.kt
+++ app/src/test/java/com/example/unahur/ContadorTest.kt
@@ -1,29 +1,31 @@
 package com.example.unahur
 
 import com.example.unahur.domain.Contador
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
-class ContadorTest : StringSpec({
-    beforeEach {
-        Contador.reiniciar()
-    }
+class ContadorTest : DescribeSpec({
+    describe("Contador") {
+        beforeEach {
+            Contador.reiniciar()
+        }
 
-    "El valor inicial es 0" {
-        Contador.valor shouldBe 0
-    }
+        it("El valor inicial es 0") {
+            Contador.valor shouldBe 0
+        }
 
-    "`incrementar` aumenta el valor en 1" {
-        Contador.incrementar()
-        Contador.valor shouldBe 1
-    }
+        it("`incrementar` aumenta el valor en 1") {
+            Contador.incrementar()
+            Contador.valor shouldBe 1
+        }
 
-    "`reiniciar` reinicia el contador a 0" {
-        Contador.incrementar()
-        Contador.incrementar()
-        Contador.valor shouldBe 2
+        it("`reiniciar` reinicia el contador a 0") {
+            Contador.incrementar()
+            Contador.incrementar()
+            Contador.valor shouldBe 2
 
-        Contador.reiniciar()
-        Contador.valor shouldBe 0
+            Contador.reiniciar()
+            Contador.valor shouldBe 0
+        }
     }
 })
